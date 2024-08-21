3ced36cfa14b057bca89a45602114876df6cbfdf
itvn9online <itvn9online@gmail.com>
2024-08-21 19:10:45 +0700

    sync

---
+++ config-sample.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * File config mẫu -> dùng khi tạo mật khẩu ngẫu nhiên
+ */
+
+// nếu ko có tham số này -> ko cho truy cập trực tiếp
+if (!defined('APP_TITLE')) {
+    die('No money no love!');
+}
